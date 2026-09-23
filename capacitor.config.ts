e3b5384b864d604f148fd7b7179e68c2227fe05e
Ibrahim Ait addimane <publicbayti@gmail.com>
2026-09-23 14:23:55 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.deroua.services',
+  appName: 'Deroua Services',
+  webDir: 'dist'
+};
+
+export default config;
